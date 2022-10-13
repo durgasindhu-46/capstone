0d273826195a0fce9bb874c6a3755269c7c5d3cd
durgasindhu-46 <68771781+durgasindhu-46@users.noreply.github.com>
2022-10-13 23:12:25 +0530

    backend

---
+++ MAadharApp/src/main/java/com/aadhar/MAadharAppApplication.java
@@ -0,0 +1,17 @@
+package com.aadhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.aadhar.bean")
+@EnableJpaRepositories(basePackages = "com.aadhar.repository")
+public class MAadharAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MAadharAppApplication.class, args);
+		System.out.println("Server running on port number 9090");
+	}
+
+}
---
+++ MAadharApp/src/main/java/com/aadhar/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.aadhar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aadhar.bean.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Integer> {
+
+}
